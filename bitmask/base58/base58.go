124c4c523a12926a6baa77e37e94c071627a42e7
jashper <tommy_a@mit.edu>
2013-08-12 19:20:10 -0400

    working on base58

---
+++ bitmask/base58/base58.go
@@ -0,0 +1,62 @@
+package base58
+
+import (
+	"errors"
+	"math/big"
+)
+
+type Base58 string
+
+const (
+	alphabet = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+)
+
+var (
+	revAlphabet = map[string]int{
+		"1": 0, "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "A": 9,
+		"B": 10, "C": 11, "D": 12, "E": 13, "F": 14, "G": 15, "H": 16, "J": 17, "K": 18,
+		"L": 19, "M": 20, "N": 21, "P": 22, "Q": 23, "R": 24, "S": 25, "T": 26, "U": 27,
+		"V": 28, "W": 29, "X": 30, "Y": 31, "Z": 32, "a": 33, "b": 34, "c": 35, "d": 36,
+		"e": 37, "f": 38, "g": 39, "h": 40, "i": 41, "j": 42, "k": 43, "m": 44, "n": 45,
+		"o": 46, "p": 47, "q": 48, "r": 49, "s": 50, "t": 51, "u": 52, "v": 53, "w": 54,
+		"x": 55, "y": 56, "z": 57,
+	}
+)
+
+func FromBytes(input []byte) (Base58, error) {
+	if len(input) < 1 {
+		return "", errors.New("base58.FromBytes: Byte slice is too short")
+	}
+
+	output := ""
+	for i := 0; i < len(input); i++ {
+		if input[i] > 0 {
+			break
+		}
+		output += "1"
+	}
+
+	n := big.NewInt(0).SetBytes(input)
+	r := big.NewInt(0)
+	base := big.NewInt(58)
+
+	zero := big.NewInt(0)
+	for n.Cmp(zero) == 1 {
+		r.Mod(n, base)
+		n.Div(n, base)
+		output += alphabet[0:r.Int64()]
+	}
+
+	return Base58(output), nil
+}
+
+func ToBytes(input Base58) ([]byte, error) {
+	output := big.NewInt(0)
+
+	//base := big.NewInt(0)
+	//exp := big.NewInt(0)
+	//for i := 0; i < len(input); i++ {
+	//	tmp.Exp(x, y, m)
+	//	output.Add(output, y)
+	//}
+}
